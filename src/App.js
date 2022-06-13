a988b6a76efcc4a1be59e595adebdf70a08d3ea9
waiz86.wa@gmail.com <humi.ahmed2017@gmail.com>
2022-06-13 16:21:09 +0100

    commit main

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
-import logo from './logo.svg';
+import { useState } from "react";
+import { useEffect } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+// const App = () => {
+//   const [zoomAnimal, setZooAnimal] =useState([]);
+//   const [error, setError] = useState(null);
+
+//   useEffect(()=>{
+//     const fetchData =async ()=> {
+//       try {
+//         const response = await fetch ("https://zoo-animal-api.herokuapp.com/animals/rand/10"  
+//         );
+//         if (!response.ok){
+//           throw new Error(response.statusText)
+//         }
+//         const data = await response.json();
+//         setZooAnimal(data);
+//         console.log(data);
+//       } catch (error) { 
+//         console.log(error);
+//         setError("Could not fetch the data");
+//     }
+//   };
+//     fetchData();
+//   },[])
+
+ 
+//   return (  
+
+//       <div className="App">
+//         <h1>Fetch API</h1>
+//         {error && <p>{error}</p>}
+//         {zoomAnimal.map((animal) => (
+//           <div key ={animal.id}>
+//           <h3>{animal.name}</h3>  
+//             <img src={animal.image_link} alt="animal"></img>
+//           </div>                     
+//         ))}
+//       </div>
+//   );
+// };
+
+const App = () => {
+  const [users, setUsers] =useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(()=>{
+    const getUsers =async ()=> {
+      try {
+        const response = await fetch ("https://fakestoreapi.com/products"  
+        );
+        if (!response.ok){
+          throw new Error(response.statusText)
+        }
+        const data = await response.json();
+        setUsers(data);
+        console.log(data);
+      } catch (error) { 
+        console.log(error);
+        setError("Could not fetch the data");
+    }
+  };
+    getUsers();
+  },[])
+
+ 
+  return (  
+
+      <div className="App">
+        <h1>Waiz clothing Store</h1>
+        {error && <p>{error}</p>}
+        {users.map((curElem) => (
+          <div key ={curElem.id}>
+          <h3>{curElem.title}</h3>  
+          <p>{curElem.price}</p>
+          <h4>{curElem.description}</h4>
+          <img src={curElem.image} alt="Men clothing"></img>
+          <p> {curElem.rating.rate}</p>
+      </div> 
+  ))}
+      </div>
   );
-}
+};
+
+
+
+
 
 export default App;
+
+
